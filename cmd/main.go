6daa2970266cb062b1e2050bfe0c256f75ff19c5
Marcelo Soto Campos <sk8sta13@gmail.com>
2025-04-27 15:24:00 -0300

    Terceiro commit

--- cmd/main.go
+++ cmd/main.go
@@ -15,28 +15,4 @@ func main() {
 
 	webserver := webserver.NewWebServer(&settings.Limits, db)
 	webserver.Start()
-
-	/*redisClient := redis.NewClient(&redis.Options{
-		Addr: fmt.Sprintf("%s:%d", settings.Redis.Host, settings.Redis.Port),
-	})
-	x := database.NewIPRedis(redisClient)
-	ipdb := dto.IPDB{Key: "172.27.0.1"}
-	x.GetData(context.Background(), &ipdb)
-	//println(ipdb)
-	fmt.Println(ipdb)*/
-
-	/*ctx := context.Background()
-	data, _ := json.Marshal(map[string]interface{}{
-		"Qtd":    12,
-		"Moment": time.Now().Unix(),
-	})
-	err := redisClient.Set(ctx, "127.0.0.1", data, 0).Err()
-	if err != nil {
-		panic(err)
-	}
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World"))
-	})
-	http.ListenAndServe(":8080", nil)*/
 }
